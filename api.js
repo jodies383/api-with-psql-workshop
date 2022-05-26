03d81f4adecb9cf5adaca4e80e2e8fb27b36ce8a
Jodie Solomons <jodies383@gmail.com>
2022-05-26 16:41:07 +0200

    remove item added

--- api.js
+++ api.js
@@ -1,3 +1,165 @@
+// module.exports = function (app, db) {
+
+// 	app.get('/api/test', function (req, res) {
+// 		res.json({
+// 			name: 'joe'
+// 		});
+// 	});
+
+// 	app.get('/api/garments', async function (req, res) {
+
+// 		// add some sql queries that filter on gender & season
+// 		const { season, gender } = req.query;
+// 		let garments
+// 		if (!gender && !season) {
+// 			garments = await db.manyOrNone(`select * from garment`);
+// 		}
+// 		else if (gender && !season) {
+// 			garments = await db.manyOrNone(`select * from garment where gender = $1`, [gender]);
+// 		}
+// 		else if (!gender && season) {
+// 			garments = await db.manyOrNone(`select * from garment where season = $1`, [season]);
+// 		}
+// 		else if (gender && season) {
+// 			garments = await db.manyOrNone(`select * from garment where season = $1 and gender = $2`, [season, gender]);
+// 		}
+
+// 		res.json({
+// 			data: garments
+// 		})
+// 	});
+
+// 	app.put('/api/garment/:id', async function (req, res) {
+
+// 		try {
+
+// 			// use an update query...
+
+// 			const { id } = req.params;
+// 			const garment = await db.many(`select * from garment where id = $1`, [id]);
+
+// 			// you could use code like this if you want to update on any column in the table
+// 			// and allow users to only specify the fields to update
+
+// 			let params = { ...garment, ...req.body };
+// 			const { description, price, img, season, gender } = params;
+// 			if (description) {
+// 				await db.oneOrNone(`update garment set description = $1 where id = $2`, [description, id])
+// 			}
+// 			else if (price) {
+// 				await db.oneOrNone(`update garment set price = $1 where id = $2`, [price, id])
+// 			}
+// 			else if (img) {
+// 				await db.oneOrNone(`update img set img = $1 where id = $2`, [img, id])
+
+// 			}
+// 			else if (season) {
+// 				await db.oneOrNone(`update garment set season = $1 where id = $2`, [season, id])
+
+// 			}
+// 			else if (gender) {
+// 				await db.oneOrNone(`update garment set gender = $1 where id = $2`, [gender, id])
+// 			}
+
+
+// 			res.json({
+// 				status: 'success'
+// 			})
+// 		} catch (err) {
+// 			console.log(err);
+// 			res.json({
+// 				status: 'error',
+// 				error: err.message
+// 			})
+// 		}
+// 	});
+
+// 	app.get('/api/garment/:id', async function (req, res) {
+
+// 		try {
+// 			const { id } = req.params;
+// 			// get the garment from the database
+// 			const garment = await db.one(`select gender from garment where id = $1`, [id]);
+
+// 			res.json({
+// 				status: 'success',
+// 				data: garment
+// 			});
+
+// 		} catch (err) {
+// 			console.log(err);
+// 			res.json({
+// 				status: 'error',
+// 				error: err.message
+// 			})
+// 		}
+// 	});
+
+
+// 	app.post('/api/garment/', async function (req, res) {
+
+// 		try {
+
+// 			const { description, price, img, season, gender } = req.body;
+// 			// insert a new garment in the database
+// 			let checkDuplicate = await db.manyOrNone(`SELECT id from garment WHERE description = $1`, [description]);
+			
+// 			if (checkDuplicate.length < 1) {
+// 				await db.none(`insert into garment (description, img, season, gender, price) values ($1,$2,$3,$4,$5)`, [description, img, season, gender, price])
+// 			}
+// 			res.json({
+// 				status: 'success'
+// 			});
+
+// 		} catch (err) {
+// 			console.log(err);
+// 			res.json({
+// 				status: 'error',
+// 				error: err.message
+// 			})
+// 		}
+// 	});
+
+// 	app.get('/api/garments/grouped', async function (req, res) {
+// 		const result = await db.many('select gender, count(*) from garment group by gender ORDER BY count(*) ASC')
+// 		// use group by query with order by asc on count(*)
+// 		res.json({
+// 			data: result
+// 		})
+// 	});
+// 	app.get('/api/garments/price/:price', async function (req, res) {
+// 		const maxPrice = Number(req.params.price);
+// 		let result
+// 		if (maxPrice > 0) {
+// 			result = await db.many('select * from garment where price <= $1', [maxPrice])
+// 		}
+// 		res.json({
+// 			data: result
+// 		})
+// 	});
+
+
+// 	app.delete('/api/garments', async function (req, res) {
+
+// 		try {
+// 			let { description } = req.query;
+// 			// delete the garments with the specified gender
+// 			await db.none(`delete from garment where description = $1`, [description])
+// 			res.json({
+// 				status: 'success'
+// 			})
+// 		} catch (err) {
+// 			// console.log(err);
+// 			res.json({
+// 				status: 'success',
+// 				error: err.stack
+// 			})
+// 		}
+// 	});
+
+
+// }
+
 module.exports = function (app, db) {
 
 	app.get('/api/test', function (req, res) {
@@ -139,12 +301,12 @@ module.exports = function (app, db) {
 	});
 
 
-	app.delete('/api/garments', async function (req, res) {
+		app.delete('/api/garments', async function (req, res) {
 
 		try {
-			let { gender } = req.query;
+			let { description } = req.query;
 			// delete the garments with the specified gender
-			await db.none(`delete from garment where gender = $1`, [gender])
+			await db.none(`delete from garment where description = $1`, [description])
 			res.json({
 				status: 'success'
 			})
--- public/app.js
+++ public/app.js
@@ -9,104 +9,127 @@ document.addEventListener('alpine:init', () => {
         seasonFilter: '',
         maxPrice: 0,
         open: false,
-        description: '', 
-        price:'', 
-        img:'', 
-        season:'', 
-        gender:'',
- 
-        toggle() {
-            this.open = ! this.open
-        },
-        getData(){
-        axios
-            .get(`/api/garments`)
-            .then(result => {
-                const results = result.data
-                this.garments=results.data
-                this.garmentsLength=results.data.length
+        description: '',
+        price: '',
+        img: '',
+        season: '',
+        gender: '',
 
-            })
+        toggle() {
+            this.open = !this.open
         },
-        getDataLength(){
+        getData() {
             axios
                 .get(`/api/garments`)
                 .then(result => {
                     const results = result.data
-                    this.garmentsLength=results.data.length
+                    this.garments = results.data
+                    this.garmentsLength = results.data.length
+
                 })
-            },
-        filterGarments(){
-            axios
-            .get(`/api/garments?gender=${this.genderFilter}&season=${this.seasonFilter}`)
-            .then(result => {
-                const results = result.data
-                this.garments=results.data
-                this.garmentsLength=results.data.length
-
-            })
         },
-        filterGarmentsByPrice(){
+        getDataLength() {
             axios
-            .get(`/api/garments/price/${this.maxPrice}`)
-            .then(result => {
-                const results = result.data
-                this.garments=results.data
-                this.garmentsLength=results.data.length
-
-            })
+                .get(`/api/garments`)
+                .then(result => {
+                    const results = result.data
+                    this.garmentsLength = results.data.length
+                })
         },
-        addGarment(){
+        filterGarments() {
+            axios
+                .get(`/api/garments?gender=${this.genderFilter}&season=${this.seasonFilter}`)
+                .then(result => {
+                    const results = result.data
+                    this.garments = results.data
+                    this.garmentsLength = results.data.length
+
+                })
+        },
+        filterGarmentsByPrice() {
+            axios
+                .get(`/api/garments/price/${this.maxPrice}`)
+                .then(result => {
+                    const results = result.data
+                    this.garments = results.data
+                    this.garmentsLength = results.data.length
+
+                })
+        },
+        addGarment() {
             const fields = {
                 description: this.description,
                 img: this.img,
                 price: this.price,
                 gender: this.gender,
                 season: this.season
-                };
-            axios
-            .post('/api/garment', fields)
-            .then(result => {
-               this.open=false
-               axios
-               .get(`/api/garments`)
-               .then(result => {
-                   const results = result.data
-                   this.garments=results.data
-               })
-               this.description= '', 
-               this.price='', 
-               this.img='', 
-               this.season='', 
-               this.gender=''
-            })
-            if (!fields === ''){
-                this.successMessage = true,
-                this.$refs.successMessage.innerText = 'garment added'
-                
+            };
+            if (this.description && this.price && this.img && this.season && this.gender !== '') {
+
+                axios
+                    .post('/api/garment', fields)
+                    .then(result => {
+                        this.open = false
+                        axios
+                            .get(`/api/garments`)
+                            .then(result => {
+                                const results = result.data
+                                this.garments = results.data
+                            })
+                        this.description = '',
+                            this.price = '',
+                            this.img = '',
+                            this.season = '',
+                            this.gender = ''
+                    })
+
             }
             else {
-                if (!this.description){
-                    this.errorMessage = true,
-                    this.$refs.errorMessage.innerText = 'missing description information'
-                } else if (!this.price){
-                    this.errorMessage = true,
-                    this.$refs.errorMessage.innerText = 'missing price information'
-                } else if (!this.img){
-                    this.errorMessage = true,
-                    this.$refs.errorMessage.innerText = 'missing img information'
-                } else if (!this.season){
-                    this.errorMessage = true,
-                    this.$refs.errorMessage.innerText = 'missing season information'
-                } else if (!this.gender){
-                    this.errorMessage = true,
-                    this.$refs.errorMessage.innerText = 'missing gender information'
-                } 
+
+                if (!fields === '') {
+                    this.successMessage = true,
+                        this.$refs.successMessage.innerText = 'garment added'
+
+                }
+                else {
+                    if (!this.description) {
+                        this.errorMessage = true,
+                            this.$refs.errorMessage.innerText = 'missing description information'
+                    } else if (!this.price) {
+                        this.errorMessage = true,
+                            this.$refs.errorMessage.innerText = 'missing price information'
+                    } else if (!this.img) {
+                        this.errorMessage = true,
+                            this.$refs.errorMessage.innerText = 'missing img information'
+                    } else if (!this.season) {
+                        this.errorMessage = true,
+                            this.$refs.errorMessage.innerText = 'missing season information'
+                    } else if (!this.gender) {
+                        this.errorMessage = true,
+                            this.$refs.errorMessage.innerText = 'missing gender information'
+                    }
+                }
             }
             setTimeout(() => { this.successMessage = false }, 2000);
             setTimeout(() => { this.errorMessage = false }, 2000);
         },
-        
+        deleteGarment(description) {
+            axios
+                .delete(`/api/garments?description=${description}`)
+                .then(result => {
+
+                })
+            axios
+                .get(`/api/garments`)
+                .then(result => {
+                    const results = result.data
+                    this.garments = results.data
+                    this.garmentsLength = results.data.length
+                    this.successMessage = true,
+                        this.$refs.successMessage.innerText = 'garment deleted'
+                    setTimeout(() => { this.successMessage = false }, 2000);
+                })
+        },
 
 
     }));
